e2911c6087f6886c7539fb7af4b1d0c3a4d89baf
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2020-02-14 16:42:05 +0100

    add separate-console utility

---
+++ separate-console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace separate_console
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                Console.Error.WriteLine("Usage: separate-console my.exe [argument]");
+                return 1;
+            }
+            var p = Process.Start(new ProcessStartInfo
+            {
+                FileName = args[0],
+                Arguments = args.Length > 1 ? $"\"{args[1]}\"" : "",
+                UseShellExecute = true,
+                CreateNoWindow = false,
+            });
+            p.WaitForExit();
+            return p.ExitCode;
+        }
+    }
+}
