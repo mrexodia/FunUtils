c4d4628a6287da2bf20f2a0c4480487e89b4d109
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2020-10-26 10:59:04 +0100

    CMakeGUI WIP

---
+++ CMakeGUI/CMakeGUI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMakeGUI
+{
+    public partial class CMakeGUI : Form
+    {
+        public CMakeGUI()
+        {
+            InitializeComponent();
+            Icon = Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        }
+    }
+}
