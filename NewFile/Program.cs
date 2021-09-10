73f18179d6c8e3ef77610ae6feb0b8571af28db4
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2021-09-10 16:13:09 +0200

    Add NewFile helper application

---
+++ NewFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.IO;
+
+namespace NewFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = Interaction.InputBox("Enter file name to create:", "Total Commander");
+            var fullPath = Path.Combine(Directory.GetCurrentDirectory(), fileName.Replace('/', Path.DirectorySeparatorChar));
+            
+            if (File.Exists(fullPath))
+            {
+                Interaction.MsgBox("File already exists!");
+                return;
+            }
+
+            var baseDirectory = Path.GetDirectoryName(fullPath);
+            Directory.CreateDirectory(baseDirectory);
+            using (var file = File.Create(fullPath))
+            {
+
+            }
+        }
+    }
+}
