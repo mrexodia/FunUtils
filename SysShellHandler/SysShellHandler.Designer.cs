fd26795354c25fdd648621147f413513e45f4a5c
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2021-12-16 15:58:41 +0100

    Add SysShellHandler utility

---
+++ SysShellHandler/SysShellHandler.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace SysShellHandler
+{
+    partial class SysShellHandler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCffExplorer = new System.Windows.Forms.Button();
+            this.buttonMakeUsermode = new System.Windows.Forms.Button();
+            this.buttonTestsign = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonCffExplorer
+            // 
+            this.buttonCffExplorer.Location = new System.Drawing.Point(12, 12);
+            this.buttonCffExplorer.Name = "buttonCffExplorer";
+            this.buttonCffExplorer.Size = new System.Drawing.Size(125, 23);
+            this.buttonCffExplorer.TabIndex = 0;
+            this.buttonCffExplorer.Text = "&CFF Explorer";
+            this.buttonCffExplorer.UseVisualStyleBackColor = true;
+            this.buttonCffExplorer.Click += new System.EventHandler(this.buttonCffExplorer_Click);
+            // 
+            // buttonMakeUsermode
+            // 
+            this.buttonMakeUsermode.Location = new System.Drawing.Point(12, 41);
+            this.buttonMakeUsermode.Name = "buttonMakeUsermode";
+            this.buttonMakeUsermode.Size = new System.Drawing.Size(125, 23);
+            this.buttonMakeUsermode.TabIndex = 1;
+            this.buttonMakeUsermode.Text = "Make&Usermode";
+            this.buttonMakeUsermode.UseVisualStyleBackColor = true;
+            this.buttonMakeUsermode.Click += new System.EventHandler(this.buttonMakeUsermode_Click);
+            // 
+            // buttonTestsign
+            // 
+            this.buttonTestsign.Location = new System.Drawing.Point(12, 70);
+            this.buttonTestsign.Name = "buttonTestsign";
+            this.buttonTestsign.Size = new System.Drawing.Size(125, 23);
+            this.buttonTestsign.TabIndex = 2;
+            this.buttonTestsign.Text = "&Testsign";
+            this.buttonTestsign.UseVisualStyleBackColor = true;
+            this.buttonTestsign.Click += new System.EventHandler(this.buttonTestsign_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(149, 107);
+            this.Controls.Add(this.buttonTestsign);
+            this.Controls.Add(this.buttonMakeUsermode);
+            this.Controls.Add(this.buttonCffExplorer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Driver Opener";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCffExplorer;
+        private System.Windows.Forms.Button buttonMakeUsermode;
+        private System.Windows.Forms.Button buttonTestsign;
+    }
+}
+
---
+++ SysShellHandler/SysShellHandler.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SysShellHandler
+{
+    public partial class SysShellHandler : Form
+    {
+        private Dictionary<char, Button> _hotkeys = new Dictionary<char, Button>();
+        private string _driverFile;
+
+        public SysShellHandler(string[] args)
+        {
+            if (args.Length > 0)
+                _driverFile = args[0];
+            else
+                _driverFile = chooseDriver();
+            InitializeComponent();
+            KeyPreview = true;
+            Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+            foreach (var c in Controls)
+            {
+                if (c is Button b)
+                {
+                    var ampIdx = b.Text.IndexOf('&');
+                    if (ampIdx != -1)
+                        _hotkeys.Add(char.ToLower(b.Text[ampIdx + 1]), b);
+                }
+            }
+
+        }
+
+        private static char ToChar(Keys key)
+        {
+            char c = '\0';
+            if ((key >= Keys.A) && (key <= Keys.Z))
+            {
+                c = (char)((int)'a' + (int)(key - Keys.A));
+            }
+
+            else if ((key >= Keys.D0) && (key <= Keys.D9))
+            {
+                c = (char)((int)'0' + (int)(key - Keys.D0));
+            }
+            else if (key == Keys.Space)
+            {
+                c = ' ';
+            }
+
+            return char.ToLower(c);
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Escape:
+                    Close();
+                    break;
+                default:
+                    var ch = char.ToLower(ToChar(e.KeyCode));
+                    if (_hotkeys.ContainsKey(ch))
+                        _hotkeys[ch].PerformClick();
+                    else
+                        base.OnKeyDown(e);
+                    break;
+            }
+        }
+
+        private string chooseDriver()
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Drivers (*.sys)|*.sys|All files (*.*)|*.*";
+                if(ofd.ShowDialog() == DialogResult.OK)
+                    return ofd.FileName;
+            }
+            Environment.Exit(1);
+            return "";
+        }
+
+        private void buttonCffExplorer_Click(object sender, EventArgs e)
+        {
+            Process.Start(Properties.Settings.Default.CffExplorer, _driverFile);
+            Environment.Exit(0);
+        }
+
+        private void buttonMakeUsermode_Click(object sender, EventArgs e)
+        {
+            var makeUsermode = Properties.Settings.Default.MakeUsermode;
+            var usermods = Path.Combine(Path.GetDirectoryName(makeUsermode), "usermode_modules");
+            foreach (var m in Directory.EnumerateFiles(usermods))
+                File.Copy(m, Path.Combine(Path.GetDirectoryName(_driverFile), Path.GetFileName(m)), true);
+            Process.Start(makeUsermode, _driverFile);
+            Environment.Exit(0);
+        }
+
+        private void buttonTestsign_Click(object sender, EventArgs e)
+        {
+            var signtool = Properties.Settings.Default.Signtool;
+            Process.Start(signtool, $"sign \"{_driverFile}\"");
+            Environment.Exit(0);
+        }
+    }
+}
